7ae8b292d969e349c43292c66f99a1baf727c846
sudarshan604 <sudarshandn1999@gmail.com>
2022-04-05 10:58:21 +0545

    --

--- module-6 next level javascript/objectliteraland for of loop/index.js
+++ module-6 next level javascript/objectliteraland for of loop/index.js
@@ -20,3 +20,40 @@ const student = [
 for (obj of student) {
   console.log(obj.name + "lives in" + obj.city);
 }
+
+const shopingList = ["eggs", "milk", "butter"];
+
+const shoppingBasket = ["potato", ...shopingList];
+
+for (shop of shoppingBasket) {
+  console.log(shop);
+}
+
+// arrow function
+
+function hello() {}
+
+// anonymous function
+const lunchMenu = function () {
+  return "";
+};
+
+// arrow function
+
+let dinnerMenu = () => {
+  return "hello ";
+};
+
+let dinnerMenu1 = (food) => ` i am going  to eat ${food}`;
+
+// but for double argumrnt we need  parenthisis
+
+let dinnerMenu2 = (food, drink) => ` i am going  to eat ${food} and ${drink}`;
+
+// default parameter challenge
+
+const grocceryShop = (drink = "something") => {
+  return `i am going to buy${drink} from the grocerry shop`;
+};
+
+console.log(grocceryShop());
