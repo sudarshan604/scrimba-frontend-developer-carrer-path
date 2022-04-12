b667e202780786442b390379b4f1a67150849724
sudarshan kshetri <sudarshandn1999@gmail.com>
2022-04-12 15:19:29 +0545

    --

--- module-7working with api/url rest&blogspace/blogspace/index.js
+++ module-7working with api/url rest&blogspace/blogspace/index.js
@@ -1,16 +1,22 @@
 var blogstore = document.getElementById("container");
 
+let postArr = [];
+
+function renderPost() {
+  blogstore.innerHTML = " ";
+  for (let i = 0; i < postArr.length; i++) {
+    blogstore.innerHTML += `<h2>${postArr[i].title}</h2>
+   <p> ${postArr[i].body}</P>
+   <hr/>
+  `;
+  }
+}
+
 fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
   .then((response) => response.json())
   .then((data) => {
-    let postArr = data.slice(0, 10);
-
-    for (let i = 0; i < postArr.length; i++) {
-      blogstore.innerHTML += `<h2>${postArr[i].title}</h2>
-       <p> ${postArr[i].body}</P>
-       <hr/>
-      `;
-    }
+    postArr = data.slice(0, 5);
+    renderPost();
   });
 
 let blog = document.getElementById("blog-contain");
@@ -21,12 +27,25 @@ blog.addEventListener("submit", (event) => {
   let formdata = new FormData(event.target);
 
   let title = formdata.get("title");
-  let titleDes = formdata.get("body");
+  let postbody = formdata.get("body");
 
   let body = {
     title: title,
-    description: titleDes,
+    body: postbody,
+  };
+  const options = {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "content-type": "application/json",
+    },
   };
 
-  console.log(body);
+  fetch("https://jsonplaceholder.typicode.com/posts", options)
+    .then((response) => response.json())
+    .then((data) => {
+      postArr.unshift(data);
+      renderPost();
+      blog.reset();
+    });
 });
--- module-7working with api/url rest&blogspace/index.js
+++ module-7working with api/url rest&blogspace/index.js
@@ -2,6 +2,13 @@
 //   .then((response) => response.json())
 //   .then((data) => console.log(data[0].name));
 
-fetch("https://jsonplaceholder.typicode.com/todos/", { method: "GET" })
-  .then((response) => response.json())
+fetch("https://jsonplaceholder.typicode.com/todos/", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "name",
+    completed: false,
+  }),
+  headers: { "Content-type": "application/json;charset=UTF-8" },
+})
+  .then((Response) => Response.json())
   .then((data) => console.log(data));
