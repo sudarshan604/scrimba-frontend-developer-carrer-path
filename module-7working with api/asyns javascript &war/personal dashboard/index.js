1e4b109773cce7f854f0a282f640c3295d81e820
sudarshan kshetri <sudarshandn1999@gmail.com>
2022-04-15 20:11:15 +0545

    --

--- module-7working with api/asyns javascript &war/personal dashboard/index.js
+++ module-7working with api/asyns javascript &war/personal dashboard/index.js
@@ -30,3 +30,54 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
   .catch((err) => {
     console.log(err);
   });
+
+let showTime = document.getElementById("displayTime");
+function displayTime() {
+  let time = new Date();
+  let hr = time.getHours();
+
+  let timeformat = "AM";
+  let minutes = time.getMinutes();
+  if (hr > 12) {
+    hr = hr - 12;
+    timeformat = "PM";
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  showTime.innerText = hr + ":" + minutes + " " + timeformat;
+}
+
+setInterval(displayTime, 1000);
+
+navigator.geolocation.getCurrentPosition((posi) => {
+  let lat = posi.coords.latitude;
+  let lon = posi.coords.longitude;
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ad40b689bbc270059e7ca074b91d0509&units=metric `
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Oops something wrong");
+      }
+      return res.json();
+    })
+    .then((dat) => {
+      console.log(dat);
+      console.log();
+      let icon_id = dat.weather[0].icon;
+      let url = `https://openweathermap.org/img/w/${icon_id}.png`;
+      console.log(url);
+
+      document.getElementById(
+        "weather-top"
+      ).innerHTML = `<img src=${url}> <p>${Math.round(
+        dat.main.temp
+      )}<sup>O</sup>c</p>`;
+      document.getElementById("weather").innerHTML += `
+         <p>${dat.name}</p>`;
+    })
+    .catch((err) => console.log(err));
+});
